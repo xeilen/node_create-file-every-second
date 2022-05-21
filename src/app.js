c1077e8afcf2bccc51938fd827dd33417f5ca294
Nazar Kovalenko <xeilen1112@gmail.com>
2022-05-21 16:24:08 +0300

    done

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const fs = require('fs');
+
+setInterval(() => {
+  const timestamp = new Date();
+  const hour = timestamp.getHours();
+  const minutes = timestamp.getMinutes();
+  const seconds = timestamp.getSeconds();
+
+  fs.appendFile(
+    `${__dirname}/app-${hour}_${minutes}_${seconds}.log`,
+    timestamp.toDateString(),
+    (err) => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.log('Error');
+
+        return;
+      }
+
+      // eslint-disable-next-line no-console
+      console.log('File has been created successful!');
+    });
+}, 1000);
